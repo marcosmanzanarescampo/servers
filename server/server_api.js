8391fedc031badf541d1b846e5c9764bb0ed9c1f
marcos <marcosmanzanarescampo@gmail.com>
2025-02-19 17:35:10 +0100

    maison

--- server/server_api.js
+++ server/server_api.js
@@ -4,13 +4,11 @@ const url = require('url'); // Le module url pour analyser les requêtes
 
 const fs = require('fs');
 const path = require('path');
+const { log } = require('console');
 const hostname = "127.0.0.1";
 const filePath = path.join(__dirname, "../data.json");
 const PORT_JSON = 4000;
 const ROUTE = '/data';
-
-
-console.log("FILE PATH:",filePath);
 
 
 // Créer le serveur
@@ -36,13 +34,11 @@ const server = http.createServer((req, res) => {
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     // Si c'est une requête GET
     if (req.method === "GET" && req.url === ROUTE) {
-        console.log(req.url);
 
         // Lire le fichier JSON
         fs.readFile(filePath, "utf8", (err, data) => {
             if (err) {
-                
-                
+                                
                 res.statusCode = 500;
                 res.end(
                     JSON.stringify({
@@ -54,14 +50,10 @@ const server = http.createServer((req, res) => {
                 res.status=200;           
                 res.setHeader("Content-Type", "application/json");
                 res.end(data || "[]");
-                console.log("Contenu JSON brut :", data);
             }
         })
     }
     else if (req.method === 'POST' && req.url === ROUTE) {
-
-            console.log('Dans le corp du POST...');
-            
 
             let body = '';
         
@@ -125,7 +117,94 @@ const server = http.createServer((req, res) => {
             //Récuperation de l'élement à mettre à jour
             // const userID = parseInt(req.url.split("/")[2], 10);
 
-            console.log("Dans le corps du PUT...");
+            let body = "";
+
+            req.on("data", chunk => {
+                body += chunk.toString();
+            });
+
+            req.on("end", () => {
+                try{
+
+                    // liste d'elements récuperés du fichier
+                    let articles = null;
+        
+                    // Lire le fichier JSON existant
+                    console.log("Reading file...");
+                    
+                    fs.readFile(filePath, "utf8", (err, data) => {
+
+                        console.log("data lu: " + data + ", type: " + typeof(data));
+                        // console.log("data lu: " + JSON.parse(data) + ", type: " + typeof(JSON.parse(data)));
+                        
+                                                
+                        if (!err && data) {
+                            try {
+                                articles = JSON.parse(data); // Parser le fichier existant
+                                console.log("Avant:");
+                                console.log(articles);
+
+                                if (!Array.isArray(articles)) {
+                                    articles = []; // S'assurer que c'est un tableau
+                                }
+
+                                //suite...
+                                
+                                // Mise à jour de l'élement
+            
+                                
+                                const {id, name, description} = JSON.parse(body);
+            
+                                if (articles.length > 0){
+                          
+                                    articles[id] = {
+                                        id: id + 1,
+                                        name: name,
+                                        description: description
+                                    };
+            
+                                }
+                                console.log("Aprés:");
+                                console.log(articles);
+                                
+
+                                // Réécrire le fichier JSON avec les nouvelles données
+                                fs.writeFile(filePath, JSON.stringify(articles, null, 2), (writeErr) => {
+                                    if (writeErr) {
+                                        res.writeHead(500, { 'Content-Type': 'application/json' });
+                                        res.end(JSON.stringify({ error: "Erreur lors de l'enregistrement des données" }));
+                                        return;
+                                    }
+                                });
+
+                                // **************************
+
+                            } catch (parseError) {
+                                console.error("Erreur de parsing JSON:", parseError);
+                                articles = [];
+                            }
+                        };
+       
+                    });
+           
+        
+
+                    res.writeHead(200, {"Content-Type": "application/json"});
+                    res.end(JSON.stringify({ message: "Données correctement mises à jour"}));
+
+                    // Mise à jour de l'élement
+
+                }catch(error){
+                    res.writeHead(400, { "Content-Type": "application/json" });
+                    res.end(JSON.stringify({ message: "Données invalides"}));
+                }
+            });
+        }
+        else if (req.method === 'DELETE' && req.url === ROUTE){
+            //Récuperation de l'élement à mettre à jour
+            // const userID = parseInt(req.url.split("/")[2], 10);
+
+            console.log("Dans le corps du DELETE...");
             
 
             let body = "";
@@ -137,64 +216,76 @@ const server = http.createServer((req, res) => {
             req.on("end", () => {
                 try{
 
-                    // console.log("Fin de chargement de donnèes:" + JSON.parse(body));
-                    console.log("données passées au PUT:" + body);
-                    
-
                     // liste d'elements récuperés du fichier
-                    let articles = [];
+                    let articles = null;
         
                     // Lire le fichier JSON existant
+                    console.log("Reading file...");
+                    
                     fs.readFile(filePath, "utf8", (err, data) => {
+
+                        console.log("data lu: " + data + ", type: " + typeof(data));
+                        // console.log("data lu: " + JSON.parse(data) + ", type: " + typeof(JSON.parse(data)));
                         
-                        console.log("readfile : " + (data));
-                        if (!err && data) {        
-                                articles = data; // Parser le fichier existant
-                                console.log("readfile articles: " + articles);                 
-                        }
+                                                
+                        if (!err && data) {
+                            try {
+                                articles = JSON.parse(data); // Parser le fichier existant
+                                // console.log("Avant:");
+                                // console.log(articles);
 
+                                if (!Array.isArray(articles)) {
+                                    articles = []; // S'assurer que c'est un tableau
+                                }
+
+                                //suite...
+                                
+                                // Mise à jour de l'élement
+            
+                                
+                                const {id, name, description} = JSON.parse(body);
+
+                                console.log("Deleting element: " + id);
+                                
+            
+                                if (articles.length > 0){
+                          
+                                    // articles[id] = {
+                                    //     id: id + 1,
+                                    //     name: name,
+                                    //     description: description
+                                    // };
+                                    // Suppresion de l'element demandé!
+                                    articles.splice(id-1, 1);
+            
+                                }
+                                console.log("Aprés:");
+                                console.log(articles);
+                                
+
+                                // Réécrire le fichier JSON avec les nouvelles données
+                                fs.writeFile(filePath, JSON.stringify(articles, null, 2), (writeErr) => {
+                                    if (writeErr) {
+                                        res.writeHead(500, { 'Content-Type': 'application/json' });
+                                        res.end(JSON.stringify({ error: "Erreur lors de l'enregistrement des données" }));
+                                        return;
+                                    }
+                                });
+
+                                // **************************
+
+                            } catch (parseError) {
+                                console.error("Erreur de parsing JSON:", parseError);
+                                articles = [];
+                            }
+                        };
+       
                     });
-
-                    console.log("Données lus du fichier de données: " + (articles));
-
-
-                    console.log("Element à mettre à jour: " + (body));
-                    
-                    
-                    // Mise à jour de l'élement
-                    
-                    const {id, name, description} = (body);
-                    
-                    console.log("index de l'élement à mettre à jour: "+ id);
-
-
-                    articles[id] = {
-                        id: id,
-                        name: name,
-                        description: description
-                    };
-
-
-
-
-                    // Réécrire le fichier JSON avec les nouvelles données
-                    fs.writeFile(filePath, JSON.stringify(articles, null, 2), (writeErr) => {
-                        if (writeErr) {
-                            res.writeHead(500, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({ error: "Erreur lors de l'enregistrement des données" }));
-                            return;
-                        }
-    
-                        // Répondre avec succès
-                        res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ message: 'Données enregistrées avec succès' }));
-                    });
-
-
+           
+        
 
                     res.writeHead(200, {"Content-Type": "application/json"});
                     res.end(JSON.stringify({ message: "Données correctement mises à jour"}));
-
 
                     // Mise à jour de l'élement
 
